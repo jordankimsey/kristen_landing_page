29b7ad2ea9b90712087dad670dc5875054e75906
Isaac Jordan kimsey <jordankimsey@gmail.com>
2022-12-17 14:57:17 -0500

    Updated Bg and bg color

--- pages/links/index.tsx
+++ pages/links/index.tsx
@@ -11,46 +11,63 @@ const index = () => {
         flexDirection: 'column',
         textAlign: 'center',
         minHeight: '100vh',
+        width: '100vw',
         pt: 8,
-        px: 2,
-        backgroundImage: "url('/bg2.jpg')",
+        backgroundImage: "url('/bg3.jpg')",
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'center',
       }}
     >
       <Box
         sx={{
           textAlign: 'center',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          px: 1,
           color: 'white',
+          display: { xs: 'block', sm: 'flex' },
+          flexDirection: { sm: 'column' },
+          alignItems: {sm: 'center'},
         }}
       >
-        <Image
-          src={'/profile_pic.jpg'}
-          alt={'profile-picture'}
-          width={120}
-          height={120}
-          style={{ borderRadius: '50%', border: '1px solid white' }}
-        />
-        <Typography sx={{ py: 2, fontWeight: 'bold' }}>
-          @kristensvintagehome
-        </Typography>
-        <LinkButton
-          text='Amazon Storefront'
-          url='https://www.amazon.com/shop/kristenkimsey'
-        />
-        <LinkButton
-          text='Chicnchill Baskets: Use code KRISTEN55'
-          url='https://www.etsy.com/shop/Chicnchilldecor?coupon=KRISTEN55'
-        />
-        <LinkButton
-          text='Cushion Lab: Use code KRISTENSVINTAGEHOME10'
-          url='https://glnk.io/6xz8w/kristensvintagehome'
-        />
-        <LinkButton
-          text='TikTok'
-          url='https://www.tiktok.com/@kristensvintagehome'
-        />
+        <Box
+          sx={{
+            backgroundColor: 'rgba(0,0,0,0.6)',
+            textAlign: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            py: 3,
+            px: {sm: 4},
+            borderRadius: '13px',
+          }}
+        >
+          <Image
+            src={'/profile_pic.jpg'}
+            alt={'profile-picture'}
+            width={120}
+            height={120}
+            style={{ borderRadius: '50%', border: '1px solid white' }}
+          />
+          <Typography sx={{ py: 2, fontWeight: 'bold' }}>
+            @kristensvintagehome
+          </Typography>
+          <LinkButton
+            text='Amazon Storefront'
+            url='https://www.amazon.com/shop/kristenkimsey'
+          />
+          <LinkButton
+            text='Chicnchill Baskets: Use code KRISTEN55'
+            url='https://www.etsy.com/shop/Chicnchilldecor?coupon=KRISTEN55'
+          />
+          <LinkButton
+            text='Cushion Lab: Use code KRISTENSVINTAGEHOME10'
+            url='https://glnk.io/6xz8w/kristensvintagehome'
+          />
+          <LinkButton
+            text='TikTok'
+            url='https://www.tiktok.com/@kristensvintagehome'
+          />
+        </Box>
       </Box>
     </Box>
   );
