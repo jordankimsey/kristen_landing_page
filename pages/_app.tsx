9ca8d37fcce882ce6c1b553880b159e3935be6ca
Jordan Kimsey <jordankimsey@gmail.com>
2026-03-30 09:53:21 -0400

    chore: upgrade to latest deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,27 @@
-import '../styles/globals.css';
+import * as React from 'react';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
+import '../styles/globals.css';
 import theme from '../theme';
+import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
+import { AppCacheProvider } from '@mui/material-nextjs/v16-pagesRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-export default function App({ Component, pageProps }: AppProps) {
+
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
   return (
-    <>
+    <AppCacheProvider {...props}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
-    </>
+    </AppCacheProvider>
   );
 }
